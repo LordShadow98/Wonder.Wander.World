4d02241a056ed05431d77131d7fd3dce4ce70750
LordShadow98 <juliancodigoc13@gmail.com>
2024-02-06 18:20:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./components/apis/Style.css"
+import Countries from './components/apis/Countries.jsx';
+
+
+
+function App() {
+  return (
+    <div className="container">     
+        <Countries/>           
+    </div>
+  );
+}
+
+export default App;
+
