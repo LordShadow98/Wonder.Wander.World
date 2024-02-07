e3a55cea23c6371bc25bfb3e7c6f45dc8dc79d93
yusleidy <yusyus8016@gmail.com>
2024-02-06 20:27:45 -0500

    Add Apis
    Integrating APIs to consume countries
    Integrating APIs to consume flags

---
+++ src/components/apis/Flags.jsx
@@ -0,0 +1,64 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ALL_COUNTRIES = gql`
+  query countries {
+    countries {
+      code
+      name
+      continent {
+        name
+      }
+      languages {
+        name
+      }
+    }
+    continents {
+      name
+    }
+  }
+`
+;
+function Countries() {
+  const { data, loading, error } = useQuery(GET_ALL_COUNTRIES);
+  if (loading) return <p>loading..</p>;
+  if (error) return <p>Error..{error.menssage}</p>;
+
+  return (
+    <div className="container">
+      <div className="row">
+        {data.countries.map((country, index) => (
+          <div className="col-md-3" key={index}>
+            <div className="border">
+              <img src="" alt=""></img>
+              {/* {country.code} */}
+              <img
+                src={"https://flagsapi.com/" + country.code + "/flat/64.png"}
+                className="marginR"/>
+              {country.name}
+              <div>
+                {/* {country.languages.map((language, index) => (
+                    <p key={index}>{language.name}</p>
+                  ))}
+                   */}
+                <p>{country.continent.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="barra">
+        <div className="row">
+          <h2>Continents</h2>
+          <div className="continents">
+            <ul className="row">
+              {data.continents.map((continent, index) => (
+                <li key={index}>{continent.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Countries;
