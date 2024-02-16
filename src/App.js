835f9305e43e6464a247f6477aa2489c4a3fd74b
sgb06 <santigallego2706@gmail.com>
2024-02-15 21:47:51 -0500

    Setting up sidebar with styles and fine-tuning frontend styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import CountryForm from "./components/crudCountry/CountryCreationForm.jsx";
 import CountryManagementForm from './components/crudCountry/CountryManagementForm.jsx';
 import Countries from './components/apis/Countries.jsx';
-
-
-
+import SideBar from "./components/sideBar/SideBar.jsx";
+import SerchBar from "./components/serchBar/SerchBar.jsx";
+import SerchBarPrueba from "./components/serchBar/Prueba.jsx";
 
 export default function App() {
   return (
-    <div className="app--container">
-      <div className="app--container--two">
-        
-        {/* <CountryForm /> */}
-        <CountryManagementForm/>
+    <Router>
+      {/* <SerchBar/> */}
+      {/* <SerchBarPrueba/> */}
+      {/* <div className="app--container"> */}
+      <div className="container-app">
+        <SideBar />
+        {/* <div className="app--container--two"> */}
+        <div className="routes">
+          <Routes>
+            <Route path="/Home" element={<Countries />} />
+            <Route path="/crear" element={<CountryForm />} />
+            <Route path="/gestionar" element={<CountryManagementForm />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
