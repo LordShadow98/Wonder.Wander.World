882ae529587b6595b7f883ddf1250ca1bf086ba0
LordShadow98 <juliancodigoc13@gmail.com>
2024-02-19 09:38:31 -0500

    Deleting unwanted files
    Files created by mistake that do not fulfill any functionality are deleted.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import CountryForm from "./components/crudCountry/CountryCreationForm.jsx";
 import CountryManagementForm from './components/crudCountry/CountryManagementForm.jsx';
-import Countries from './components/apis/Countries.jsx';
 import SideBar from "./components/sideBar/SideBar.jsx";
 
 
@@ -15,7 +14,7 @@ export default function App() {
         <div className="app--container--two">
           <div className="routes">
             <Routes>
-              <Route path="/Home" element={<Countries />} />
+              {/* <Route path="/Home" element={<Countries />} /> */}
               <Route path="/crear" element={<CountryForm />} />
               <Route path="/gestionar" element={<CountryManagementForm />} />
             </Routes>
