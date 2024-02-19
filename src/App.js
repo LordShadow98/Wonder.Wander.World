811db0d99718756c93cce8ddbfbccb3482a56674
LordShadow98 <juliancodigoc13@gmail.com>
2024-02-19 09:06:23 -0500

    Add small sidebar css changes

--- src/App.js
+++ src/App.js
@@ -3,57 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import CountryForm from "./components/crudCountry/CountryCreationForm.jsx";
 import CountryManagementForm from './components/crudCountry/CountryManagementForm.jsx';
+import Countries from './components/apis/Countries.jsx';
 import SideBar from "./components/sideBar/SideBar.jsx";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import SerchBar from "./components/serchBar/SerchBar.jsx";
-import Countries from "./components/urls/Countries.jsx";
-=======
->>>>>>> 81f30f4 ("Adding search bar with its respective styles and functionalities.")
-=======
->>>>>>> 81f30f497722056027d36e8961eca004a8b4f890
 
 export default function App() {
   return (
     <Router>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <SerchBar/>    
-        <div className="container-app">
-          <SideBar />
-          <div className="app--container--two">
-            <div className="routes">
-              <Routes>
-                <Route path="/Home" element={<Countries />} />
-                <Route path="/crear" element={<CountryForm />} />
-                <Route path="/gestionar" element={<CountryManagementForm />} />
-              </Routes>
-            </div>
+      <div className="container-app">
+        <SideBar />
+        <div className="app--container--two">
+          <div className="routes">
+            <Routes>
+              <Route path="/Home" element={<Countries />} />
+              <Route path="/crear" element={<CountryForm />} />
+              <Route path="/gestionar" element={<CountryManagementForm />} />
+            </Routes>
           </div>
         </div>
-    
-=======
-=======
->>>>>>> 81f30f497722056027d36e8961eca004a8b4f890
-      
-      {/* <SerchBarPrueba/> */}
-      {/* <div className="app--container"> */}
-      <main className="container-app">
-        <SideBar />
-        
-        {/* <div className="app--container--two"> */}
-        <div className="routes">
-          <Routes>
-            <Route path="/Home" element={<Countries />} />
-            <Route path="/crear" element={<CountryForm />} />
-            <Route path="/gestionar" element={<CountryManagementForm />} />
-          </Routes>
-        </div>
-      </main>
-<<<<<<< HEAD
->>>>>>> 81f30f4 ("Adding search bar with its respective styles and functionalities.")
-=======
->>>>>>> 81f30f497722056027d36e8961eca004a8b4f890
+      </div>
     </Router>
   );
 }
--- src/components/serchBar/SerchBar.jsx
+++ src/components/serchBar/SerchBar.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-import "./SerchBar.css";
+// import React, { useState } from "react";
+// import "./SerchBar.css";
 
-const SerchBar = () => {
-  const list = [];
-  const [filterList, setFilterList] = useState(list);
+// const SerchBar = () => {
+//   const list = [];
+//   const [filterList, setFilterList] = useState(list);
 
-  const handleSearch = (event) => {
-    if (event.target.value === "") {
-      setFilterList(list);
-    }
-    const filteredList = list.filter(
-        (item) =>
-          item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
-      );
-      setFilterList(filteredList);
-  };
-  return (
-    <div className="Search">
-      <input
-        name="quey"
-        type="text"
-        placeholder="Search"
-        onChange={handleSearch}
-      />
-      {filterList &&
-        filterList.map((item, index) => <div key={index}>{item}</div>)}
-    </div>
-  );
-};
+//   const handleSearch = (event) => {
+//     if (event.target.value === "") {
+//       setFilterList(list);
+//     }
+//     const filteredList = list.filter(
+//         (item) =>
+//           item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+//       );
+//       setFilterList(filteredList);
+//   };
+//   return (
+//     <div className="Search">
+//       <input
+//         name="quey"
+//         type="text"
+//         placeholder="Search"
+//         onChange={handleSearch}
+//       />
+//       {filterList &&
+//         filterList.map((item, index) => <div key={index}>{item}</div>)}
+//     </div>
+//   );
+// };
 
-export default SerchBar;
+// export default SerchBar;
